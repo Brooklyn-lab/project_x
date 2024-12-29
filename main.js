45ea843e87b2566ec96966376edeb7785e035a26
Ilya Stepaniuk <ilya.stepaniuk@patrianna.com>
2024-12-29 15:58:24 +0200

    add ua page, change text for both pages

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //   new Glide(".glide-works", {
-  //     type: "carousel",
-  //     768: { perView: 1.2 },
-  //     991.98: { perView: 3 },
-  //     perView: 4.5,
-  //     gap: 16,
-  //   }).mount()
+  const langLinks = document.querySelectorAll(".lang-switcher__link")
+
+  const currentUrl = window.location.pathname
+
+  langLinks.forEach((link) => {
+    const linkHref = link.getAttribute("href")
+
+    if (linkHref === currentUrl) {
+      link.classList.add("lang-switcher__link_active")
+    } else {
+      link.classList.remove("lang-switcher__link_active")
+    }
+  })
+
+  new Glide(".glide-works", {
+    type: "carousel",
+    perView: 3.5,
+    gap: 12,
+    breakpoints: {
+      991.98: { perView: 2.5 },
+      599.98: { perView: 1.4, gap: 8 },
+    },
+  }).mount()
 
   new Glide(".glide-certificates", {
     type: "carousel",
-    perView: 4.5,
+    perView: 4.2,
     breakpoints: {
       991.98: { perView: 2.5 },
-      479.98: { perView: 1.5 },
+      479.98: { perView: 1.4 },
     },
     gap: 16,
   }).mount()
@@ -21,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector("body")
 
   burger.addEventListener("click", (event) => {
-    event.stopPropagation() // Запобігаємо спрацюванню події на document
+    event.stopPropagation()
     body.classList.toggle("nav-visible")
   })
 
@@ -29,7 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".nav")
     const body = document.body
 
-    // Перевіряємо, чи клік був не по навігації або по посиланню в навігації
     if (!nav.contains(event.target) || event.target.closest(".nav__link")) {
       body.classList.remove("nav-visible")
     }
@@ -37,35 +52,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const items = document.querySelectorAll(".accordion button")
 
-  // Функція для закриття всіх акардіонів
   function closeAllAccordions() {
     items.forEach((item) => item.setAttribute("aria-expanded", "false"))
   }
 
-  // Функція для переключення акардіону
   function toggleAccordion(event) {
     const itemToggle = this.getAttribute("aria-expanded")
 
-    // Закриваємо всі акардіони
     closeAllAccordions()
 
-    // Відкриваємо, якщо раніше був закритий
     if (itemToggle === "false") {
       this.setAttribute("aria-expanded", "true")
     }
 
-    // Зупиняємо поширення події, щоб не викликати закриття акардіона при кліку на кнопці
     event.stopPropagation()
   }
 
-  // Додаємо обробник до кожної кнопки акардіону
   items.forEach((item) => {
     item.addEventListener("click", toggleAccordion)
   })
 
-  // Закриття акардіону при кліку поза його межами
   document.addEventListener("click", (event) => {
-    // Перевіряємо, чи клік був за межами акардіона
     if (!event.target.closest(".accordion")) {
       closeAllAccordions()
     }
@@ -75,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelector(".nav").addEventListener("click", function (event) {
     const link = event.target.closest("a.nav__link")
-    if (!link) return // Якщо клік не по посиланню, виходимо
+    if (!link) return
 
     event.preventDefault()
 
@@ -88,7 +95,6 @@ document.addEventListener("DOMContentLoaded", () => {
         block: "start",
       })
 
-      // Якщо body має клас nav-visible (мобільне меню), прибираємо його
       if (document.body.classList.contains("nav-visible")) {
         document.body.classList.remove("nav-visible")
       }
